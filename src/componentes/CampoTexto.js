c4fbe3a4d6c58e89942738c6d8f49d3c58639aa6
Alberto Valdez <tico.urquhart@gmail.com>
2022-10-31 20:27:38 -0300

    commit inicial

---
+++ src/componentes/CampoTexto.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { TextInput as Input } from 'react-native-paper'
+import { theme } from '../core/theme'
+
+export default function CampoTexto({ errorText, description, ...props }) {
+  return (
+    <View style={styles.contenedor}>
+      <Input
+        style={styles.input}
+        selectionColor={theme.colors.primario}
+        underlineColor="transparent"
+        mode="outlined"
+        {...props}
+      />
+      {description && !errorText ? (
+        <Text style={styles.description}>{description}</Text>
+      ) : null}
+      {errorText ? <Text style={styles.error}>{errorText}</Text> : null}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  contenedor: {
+    width: '100%',
+    marginVertical: 12,
+  },
+  input: {
+    backgroundColor: theme.colors.surface,
+  },
+  description: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+    paddingTop: 8,
+  },
+  error: {
+    fontSize: 13,
+    color: theme.colors.error,
+    paddingTop: 8,
+  },
+})
