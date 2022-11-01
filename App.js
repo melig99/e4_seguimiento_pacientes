8d463d07fb0194a7670e22b0933f13640e6af947
Alberto Valdez <tico.urquhart@gmail.com>
2022-10-31 22:02:38 -0300

    Se agrega los archivos base y navegacion al proyecto

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen,
+         Pacientes
+ } from './src/screen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Prueba de Aplicacion</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/screen/index.js
@@ -0,0 +1,2 @@
+export { default as HomeScreen } from './Home'
+export { default as Pacientes } from './Pacientes'
