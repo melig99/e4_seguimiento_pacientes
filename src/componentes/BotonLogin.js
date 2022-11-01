c5809f16b3a1694081fec4cf8c6b841ec1d01e9f
melig99 <56968146+melig99@users.noreply.github.com>
2022-11-01 00:52:23 -0300

    Actualizacion de Login

--- src/componentes/BotonLogin.js
+++ src/componentes/BotonLogin.js
@@ -1,37 +1,13 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Button } from 'react-native';
 import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
+import {tema} from '../tema/tema'
 
 export default function BotonLogin ({evento}){
     return(
-        <TouchableOpacity style={styles.container} onPress={evento}>
-             <LinearGradient
-                    colors={['#FFB677', '#FF3CBD']}
-                    start={{x:1, y:0}}
-                    end={{x:0, y:1}}
-                    style={styles.button}>
-                <Text style={styles.text}>Ingresar</Text>
-            </LinearGradient>
+        <TouchableOpacity onPress={evento}>
+            <Button title="Ingresar"
+                color={tema.colors.primario}/>
         </TouchableOpacity>
     );
+
 }
-
-const styles = StyleSheet.create({
-
-    text: {
-        fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold',
-
-
-    },
-    button: {
-        width: '80%',
-        height: 50,
-        borderRadius: 25,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-
-    }
-});
