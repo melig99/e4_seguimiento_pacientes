0f717623b9918c09b915c133a549c590f3bfb569
Alberto Valdez <tico.urquhart@gmail.com>
2022-11-06 17:48:36 -0300

    Creacion de archivo peticiones para las peticiones a la API correspondiente.

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,22 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
-import * as React from 'react';
+import React,{ useState } from 'react';
 import BotonLogin from '../componentes/BotonLogin'
+import {peticionesGet} from '../core/peticiones'
 
 export default function Login({navigation}) {
+    const [usuario, setUsuario] = useState('');
+    let error="";
+
+    const loguear= async ()=>{
+        console.log(`{"soloUsuariosDelSistema":true,"usuarioLogin":"${usuario}"}`)
+        const resp =  await peticionesGet('persona',{"ejemplo":`{"soloUsuariosDelSistema":true,"usuarioLogin":"${usuario}"}`});
+        console.log(resp)
+        if(resp.cod ==0){
+            console.log(resp.respuesta)
+            navigation.reset({index: 0,routes: [{ name: 'Home' }],})
+        }else{
+            console.log("Error")
+        }
+    }
+
   return (
     <View style={styles.container}>
         <Text style={styles.titulo}>INICIO SESION</Text>
         <Text style={styles.subTitulo}>Inicie sesión en su cuenta</Text>
-        <TextInput placeholder="jhon@mail.com" style={styles.textInput}/>
+        <TextInput placeholder="jhon@mail.com" style={styles.textInput} onChangeText ={(valor)=>{setUsuario(valor)}} />
         <TextInput placeholder="contraseña" style={styles.textInput}/>
         <Text/>
-        <BotonLogin evento={() =>navigation.reset({index: 0,routes: [{ name: 'Home' }],})}/>
+        <BotonLogin evento={() => loguear()}/>
+        <Text>{error}</Text>
         <StatusBar style="auto" />
     </View>
   );
 }
-
+// <BotonLogin evento={() =>navigation.reset({index: 0,routes: [{ name: 'Home' }],})}/>
 const styles = StyleSheet.create({
     container: {
         flex: 1,
